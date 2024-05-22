96c53d8d4772e24433519bd92cad6f6566d3c5cd
chmuhammadasim <boybrave82@gmail.com>
2024-05-22 09:49:03 +0500

    chore: Update Dappcord contract with channel creation and joining functionality

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,28 +1,28 @@
-const ethers = require("hardhat")
+const hre = require("hardhat");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
+  return hre.ethers.utils.parseUnits(n.toString(), 'ether');
 }
 
 async function main() {
-  const [deployer] = await ethers.getSigners()
-  const NAME = "Dappcord"
-  const SYMBOL = "DC"
+  const [deployer] = await hre.ethers.getSigners();
+  const NAME = "Dappcord";
+  const SYMBOL = "DC";
 
-  const Dappcord = await ethers.getContractFactory("Dappcord")
-  const dappcord = await Dappcord.deploy(NAME, SYMBOL)
-  await dappcord.deployed()
+  const Dappcord = await hre.ethers.getContractFactory("Dappcord");
+  const dappcord = await Dappcord.deploy(NAME, SYMBOL);
+  await dappcord.deployed();
 
-  console.log(`Deployed Dappcord Contract at: ${dappcord.address}\n`)
+  console.log(`Deployed Dappcord Contract at: ${dappcord.address}\n`);
 
-  const CHANNEL_NAMES = ["general", "intro", "jobs"]
-  const COSTS = [tokens(1), tokens(0), tokens(0.25)]
+  const CHANNEL_NAMES = ["general", "intro", "jobs"];
+  const COSTS = [tokens(1), tokens(0), tokens(0.25)];
 
-  for (var i = 0; i < 3; i++) {
-    const transaction = await dappcord.connect(deployer).createChannel(CHANNEL_NAMES[i], COSTS[i])
-    await transaction.wait()
+  for (let i = 0; i < CHANNEL_NAMES.length; i++) {
+    const transaction = await dappcord.connect(deployer).createChannel(CHANNEL_NAMES[i], COSTS[i]);
+    await transaction.wait();
 
-    console.log(`Created text channel #${CHANNEL_NAMES[i]}`)
+    console.log(`Created text channel #${CHANNEL_NAMES[i]}`);
   }
 }
 
